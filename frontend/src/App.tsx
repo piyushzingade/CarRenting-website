a90e684c5ddeadcddc5a1914bbc245b45b1d70d6
Piyushzingade <piyushzingade@gmail.com>
2025-02-06 23:46:40 +0530

    Added Appbar and navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
-
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import User from "./pages/User";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
-    <div className=" flex justify-center bg-amber-300 items-center h-40 w-40 rounded-2xl shadow-orange-300">
-     
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user" element={<User />} />
+        {/* <Route path="/booking" element={<BookingPage />} /> */}
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Appbar.tsx
@@ -0,0 +1,45 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass, faUser } from "@fortawesome/free-solid-svg-icons"; 
+// import axios from "axios";
+// import { BACKEND_URL } from "../lib/config";
+import { useNavigate } from "react-router-dom";
+
+export const Appbar = () => {
+    const navigate = useNavigate()
+    
+
+    const openAccount = () => {
+        navigate("/user")
+        // const response = await axios.get(`${BACKEND_URL}/user/profile`)
+        // const data  = response.data.user;
+        // return data;
+    }
+
+    return (
+    <div className=" flex justify-between items-center border-b-2 shadow-xl border-gray-100 px-5">
+      <div className="p-4 ">
+        <h1 className="text-blue-500 font-bold text-2xl">CarRent</h1>
+      </div>
+      <div className="flex justify-between items-center gap-4">
+        <div className="hover:cursor-pointer hover:underline">Home </div>
+        <div className="hover:cursor-pointer hover:underline">Cars</div>
+        <div className="hover:cursor-pointer hover:underline">Tour Packages</div>
+        <div className="hover:cursor-pointer hover:underline">Offer</div>
+        <div className="hover:cursor-pointer hover:underline">Contact</div>
+      </div>
+      <div className="flex justify-between items-center gap-5">
+        <div className="hover:cursor-pointer">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </div>
+        <div className="hover:cursor-pointer" onClick={openAccount}>
+          <FontAwesomeIcon icon={faUser} className="light" />
+        </div>
+        <div className="pr-3">
+          <button className="bg-white border border-gray-600 hover:bg-blue-600 hover:text-white p-2 rounded-2xl">
+            Book Now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
