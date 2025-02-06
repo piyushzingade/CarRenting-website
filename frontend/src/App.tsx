b18ae1bb5daf7d8523499c5cb831cb37f4850118
Piyushzingade <piyushzingade@gmail.com>
2025-02-06 11:34:35 +0530

    updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className=" flex justify-center bg-amber-300 items-center h-40 w-40 rounded-2xl shadow-orange-300">
